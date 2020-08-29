f64794821fc655fa2c40174be9d441da1b297a31
Hackerbot1488 <gromovartem04@mail.ru>
2020-08-29 10:53:57 +0300

    made it

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
 $(document).ready(function () {
   const mMenuBtn = $('.mobile-menu-button')
   const mMenu = $('.m-menu')
+  const tab = $('.tab')
   mMenuBtn.on('click', function () {
     mMenu.toggleClass('m-menu_active')
     $('body').toggleClass('no-scroll')
   });
+  tab.on('click', function () {
+    tab.removeClass('cast__title_active')
+    $(this).toggleClass('cast__title_active')
+    let activeTabContent = $(this).attr('data-target')
+    $('.tabs__content').removeClass('tabs__content_visible')
+    $(activeTabContent).toggleClass('tabs__content_visible')
+  })
   const mySwiper = new Swiper('.swiper-container', {
     slidesPerView: 4,
     spaceBetween: 25,
@@ -16,7 +24,10 @@ $(document).ready(function () {
         slidesPerView: 2
       },
       320: {
-        slidesPerView: 1
+        slidesPerView: 1,
+        navigation: {
+          nextEl: '.button-next'
+        }
       }
     },
     loop: true,
